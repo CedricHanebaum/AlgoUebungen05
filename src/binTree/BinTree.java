f57d6fd62dfb6962f56f3055ccc009fc38adeb45
CedricHanebaum <Cedric.Hanebaum@rwth-aachen.de>
2014-04-15 14:42:58 +0200

    completed 05

--- src/binTree/BinTree.java
+++ src/binTree/BinTree.java
@@ -1,5 +1,7 @@
 package binTree;
 
+import java.util.Random;
+
 public class BinTree {
 
 	private int value;
@@ -11,14 +13,72 @@ public class BinTree {
 	public BinTree(int value) {
 		this.value = value;
 	}
-	
-	public BinTree(int[] values){
-		if(values.length == 0) throw new IllegalArgumentException();
+
+	public BinTree(int[] values) {
+		if (values.length == 0)
+			throw new IllegalArgumentException();
 		this.value = values[0];
-		
-		for(int i = 1; i < values.length; ++i){
+
+		for (int i = 1; i < values.length; ++i) {
 			this.insert(values[i]);
 		}
+	}
+	
+	public BinTree(int count, int min, int max){
+		if(count == 0) throw new IllegalArgumentException();
+		Random r = new Random();
+		for(int i = 1; i < count; ++i){
+			this.insert(r.nextInt(min + max + 1) - min);
+		}
+	}
+
+	public int getNodeCount() {
+		if (isLeaf()){
+			return 1;
+		}
+		int leftc = 0, rightc = 0;
+		if(left != null){
+			leftc = left.getNodeCount();
+		}
+		if(right != null){
+			rightc = right.getNodeCount();
+		}
+		return 1 + leftc + rightc;
+	}
+
+	public int getHeight() {
+		if (isLeaf()){
+			return 1;
+		}
+		if(left == null) return right.getHeight() + 1;
+		if(right == null) return left.getHeight() + 1;
+		if (left.getHeight() > right.getHeight()) {
+			return left.getHeight() + 1;
+		}
+		return right.getHeight() + 1;
+		
+	}
+	
+	public boolean isComplete(){
+		return isComplete(this.getHeight());
+	}
+
+	private boolean isComplete(int height) {
+		if((left == null) || (right == null)) return false;
+		if(height == 2){
+			if(!left.isLeaf() || !right.isLeaf()) return false;
+			return true;
+		}
+		return left.isComplete(height - 1) && right.isComplete(height - 1);
+	}
+	
+	public boolean isAVL(){
+		if(isLeaf()) return true;
+		if(left != null && !left.isAVL()) return false;
+		if(right != null && !right.isAVL()) return false;
+		if(left == null) return right.getHeight() < 2;
+		if(right == null) return left.getHeight() < 2;
+		return Math.abs(left.getHeight() - right.getHeight()) < 2;
 	}
 
 	public BinTree getNode(int x) {
@@ -87,17 +147,17 @@ public class BinTree {
 			} else {
 				BinTree t = this.getSmallestGreater();
 				t.parent.left = t.right;
-				
-				if(t.parent.left == t) t.parent.left = null;
-				if(t.parent.right == t) t.parent.right = null;
+
+				if (t.parent.left == t)
+					t.parent.left = null;
+				if (t.parent.right == t)
+					t.parent.right = null;
 
 				if (left != null)
 					left.parent = t;
 				if (right != null)
 					right.parent = t;
 
-				
-				
 				t.left = left;
 				t.right = right;
 			}
@@ -126,8 +186,8 @@ public class BinTree {
 
 	@Override
 	public String toString() {
-		return value + "; " + (left == null ? ""
-				: left.toString()) + (right == null ? "" : right.toString());
+		return value + "; " + (left == null ? "" : left.toString())
+				+ (right == null ? "" : right.toString());
 	}
 
 	public static void main(String[] args) {
@@ -142,15 +202,25 @@ public class BinTree {
 		t.insert(13);
 		t.insert(17);
 		t.insert(20);
-		t.insert(25);
-		t.insert(23);
-		t.insert(28);
+		// t.insert(25);
+		// t.insert(23);
+		// t.insert(28);
 
 		System.out.println(t);
+
+		System.out.println(t.getHeight());
+		System.out.println(t.getNodeCount());
+		System.out.println(t.isComplete());
+		System.out.println(t.isAVL());
+		System.out.println("-----------");
+		 
+		t = new BinTree(10);
+		t.insert(5);
+		t.insert(15);
 		
-		t.remove(18);
-		
+		System.out.println(t.isComplete());
 		System.out.println(t);
+		System.out.println(t.isAVL());
 
 	}
 
